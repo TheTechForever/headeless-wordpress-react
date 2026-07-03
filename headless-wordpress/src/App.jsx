4dba702cd75082af276935b53f4c2b91e2bd388b
Tejas Panchal <tejaskumar.panchal1989@gmail.com>
2026-07-03 12:00:05 +0530

    first update

--- headless-wordpress/src/App.jsx
+++ headless-wordpress/src/App.jsx
@@ -10,6 +10,8 @@ function App() {
   return (
     <>
       <section id="center">
+
+        <h1>Tejas</h1>
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
