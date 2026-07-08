277b2f82bcc0f79c510df97bd9cb6b47ac3ab74f
Tejas Panchal <tejaskumar.panchal1989@gmail.com>
2026-07-08 13:19:40 +0530

    aaa

--- headless-frontend/src/app/page.tsx
+++ headless-frontend/src/app/page.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <main className="p-10">
       <h1 className="text-4xl font-bold">
-        Welcome to Tejas Headless WordPress 🚀
+        Welcome to Headless WordPress 🚀
       </h1>
-
+      <h2>Headless WordPress Frontend</h2>
       <p className="mt-4 text-lg">
         Learning Next.js step by step.
       </p>
