928cd5f9709836890b96ca123fcca1a70c96b389
Tejas Panchal <tejaskumar.panchal1989@gmail.com>
2026-07-08 13:44:24 +0530

    start Home page development

---
+++ headless-frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-900 text-white p-5 mt-20">
+      <div className="container mx-auto text-center">
+        © 2026 Headless WordPress
+      </div>
+    </footer>
+  );
+}
---
+++ headless-frontend/src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="bg-slate-900 text-white p-5">
+      <div className="container mx-auto">
+        <h2 className="text-2xl font-bold">
+          Headless WordPress
+        </h2>
+      </div>
+    </header>
+  );
+}
