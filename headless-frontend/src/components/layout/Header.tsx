597d9eb8ffdffd61bb6ac73114e8fd6b4b558b9d
Tejas Panchal <tejaskumar.panchal1989@gmail.com>
2026-07-10 15:38:34 +0530

    aaa

--- headless-frontend/src/components/layout/Header.tsx
+++ headless-frontend/src/components/layout/Header.tsx
@@ -1,11 +1,7 @@
 export default function Header() {
   return (
-    <header className="bg-slate-900 text-white p-5">
-      <div className="container mx-auto">
-        <h2 className="text-2xl font-bold">
-          Headless WordPress
-        </h2>
-      </div>
+    <header>
+      <h1>eHub</h1>
     </header>
   );
 }
