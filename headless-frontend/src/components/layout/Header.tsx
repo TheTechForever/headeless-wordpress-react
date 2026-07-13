30b9a025d3cc8a01abd445a7a033b512b2eb1dc5
Tejas Panchal <tejaskumar.panchal1989@gmail.com>
2026-07-13 16:55:01 +0530

    aaa

--- headless-frontend/src/components/layout/Header.tsx
+++ headless-frontend/src/components/layout/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <header>
-      <h1 className="text-3xl text-center font-bold">eHub</h1>
+      <div className="bg-slate-900 text-white p-5">
+        <h1 className="text-3xl text-center font-bold">eHub</h1>
+      </div>
     </header>
   );
 }
